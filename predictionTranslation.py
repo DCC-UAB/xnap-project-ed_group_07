d240b8f79b59bf38c22a9975b7dd9ec35348d39f
Alba Ballarà <aballara3@gmail.com>
2023-05-19 14:27:58 +0000

    prediction changed

--- predictionTranslation.py
+++ predictionTranslation.py
@@ -1,10 +1,10 @@
 from keras.models import load_model
 from util import *
-filename="char2encoding.pkl"
+filename="./output/char2encoding.pkl"
 sentence="I work"
 #saveChar2encoding("char2encoding.pkl",input_token_index,16,71,reverse_target_char_index,num_decoder_tokens,target_token_index)
 input_token_index,max_encoder_seq_length,num_encoder_tokens,reverse_target_char_index,num_decoder_tokens,target_token_index= getChar2encoding(filename)
-encoder_input_data=encodingSentenceToPredict(sentence,input_token_index,16,71)
+encoder_input_data=encodingSentenceToPredict(sentence,input_token_index,16,72)
 encoder_model= load_model('encoder_modelPredTranslation.h5')
 decoder_model= load_model('decoder_modelPredTranslation.h5')
 
