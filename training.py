89ad78506d8a387c4c33e0c04526ed44b1da4288
Berta <1600460@uab.cat>
2023-05-21 17:01:57 +0000

    Canvis batches

--- training.py
+++ training.py
@@ -1,19 +1,26 @@
 from util import *
 
-# batch_inici = 0
-# batch_final = 10
-#load the data and format  them for being processed
-encoder_input_data, decoder_input_data, decoder_target_data, input_token_index, target_token_index,input_texts,target_texts,num_encoder_tokens,num_decoder_tokens,num_decoder_tokens,max_encoder_seq_length=prepareData(data_path, bacth_inici, batch_final)
+batch_inici = 0
+batch_final = num_samples
+data_path = './spa-eng/spa.txt' #139705
+lines = open(data_path).read().split('\n')
+batches = ((len(lines)-1)//num_samples)+1 #afegim un batch més per als que sobren
 
-# we build the model
-model,decoder_outputs,encoder_inputs,encoder_states,decoder_inputs,decoder_lstm,decoder_dense=modelTranslation(num_encoder_tokens,num_decoder_tokens)
+for i in range (batches):
+    #load the data and format  them for being processed
+    encoder_input_data, decoder_input_data, decoder_target_data, input_token_index, target_token_index,input_texts,target_texts,num_encoder_tokens,num_decoder_tokens,num_decoder_tokens,max_encoder_seq_length=prepareData(data_path, batch_inici, batch_final)
 
-# we train it
-trainSeq2Seq(model,encoder_input_data, decoder_input_data,decoder_target_data)
+    # we build the model
+    model,decoder_outputs,encoder_inputs,encoder_states,decoder_inputs,decoder_lstm,decoder_dense=modelTranslation(num_encoder_tokens,num_decoder_tokens)
 
-# we build the final model for the inference (slightly different) and we save it
-encoder_model,decoder_model,reverse_target_char_index=generateInferenceModel(encoder_inputs, encoder_states,input_token_index,target_token_index,decoder_lstm,decoder_inputs,decoder_dense)
+    # we train it
+    trainSeq2Seq(model,encoder_input_data, decoder_input_data,decoder_target_data)
 
+    # we build the final model for the inference (slightly different) and we save it
+    encoder_model,decoder_model,reverse_target_char_index=generateInferenceModel(encoder_inputs, encoder_states,input_token_index,target_token_index,decoder_lstm,decoder_inputs,decoder_dense)
+
+    batch_inici += num_samples
+    batch_final += num_samples
 # we save the object to convert the sequence to encoding  and encoding to sequence
 # our model is made for being used with different langages that do not have the same number of letters and the same alphabet
 saveChar2encoding("./output/char2encoding.pkl",input_token_index,max_encoder_seq_length,num_encoder_tokens,reverse_target_char_index,num_decoder_tokens,target_token_index)
