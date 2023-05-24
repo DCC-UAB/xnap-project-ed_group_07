95a7b3b3a94135fae9a24eab410c5a4025d60c2f
blancadejuan <1604365@uab.cat>
2023-05-24 16:17:30 +0000

    canvis classe

--- training.py
+++ training.py
@@ -6,27 +6,34 @@ data_path = './spa-eng/spa.txt' #139705
 lines = open(data_path).read().split('\n')
 batches = ((len(lines)-1)//num_samples)+1 #afegim un batch més per als que sobren
 
+#tokens no depen de les dades ino de dataset
+#guardar fora dsp i model transl fora?
+
+
+# we build the model
+model,decoder_outputs,encoder_inputs,encoder_states,decoder_inputs,decoder_lstm,decoder_dense=modelTranslation(num_encoder_tokens,num_decoder_tokens)
+    
 
 for i in range (batches):
-    num_encoder_tokens_ant = num_encoder_tokens
-    num_decoder_tokens_ant = num_decoder_tokens
+    #if i != 0:
+        #num_encoder_tokens_ant = num_encoder_tokens
+        #num_decoder_tokens_ant = num_decoder_tokens
+    
     #load the data and format  them for being processed
     encoder_input_data, decoder_input_data, decoder_target_data, input_token_index, target_token_index,input_texts,target_texts,num_encoder_tokens,num_decoder_tokens,num_decoder_tokens,max_encoder_seq_length=prepareData(data_path, batch_inici, batch_final)
-    if num_encoder_tokens < num_encoder_tokens_ant:
-        num_decoder_tokens= num_encoder_tokens_ant
-    if num_decoder_tokens < num_decoder_tokens:
-        num_decoder_tokens = num_decoder_tokens_ant
-    # we build the model
-    model,decoder_outputs,encoder_inputs,encoder_states,decoder_inputs,decoder_lstm,decoder_dense=modelTranslation(num_encoder_tokens,num_decoder_tokens)
-
+    #if i != 0:
+        #if num_encoder_tokens < num_encoder_tokens_ant:
+            #num_decoder_tokens= num_encoder_tokens_ant
+        #if num_decoder_tokens < num_decoder_tokens:
+            #num_decoder_tokens = num_decoder_tokens_ant
     # we train it
     trainSeq2Seq(model,encoder_input_data, decoder_input_data,decoder_target_data)
 
-    # we build the final model for the inference (slightly different) and we save it
-    encoder_model,decoder_model,reverse_target_char_index=generateInferenceModel(encoder_inputs, encoder_states,input_token_index,target_token_index,decoder_lstm,decoder_inputs,decoder_dense)
-
     batch_inici += num_samples
     batch_final += num_samples
+
+# we build the final model for the inference (slightly different) and we save it
+encoder_model,decoder_model,reverse_target_char_index=generateInferenceModel(encoder_inputs, encoder_states,input_token_index,target_token_index,decoder_lstm,decoder_inputs,decoder_dense)
 
 # we save the object to convert the sequence to encoding  and encoding to sequence
 # our model is made for being used with different langages that do not have the same number of letters and the same alphabet
