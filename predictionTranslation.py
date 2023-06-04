e8a42a5f47a1c70ddc6417ab666d3782c0c1058d
albaballara <101924249+albaballara@users.noreply.github.com>
2023-06-04 11:56:24 +0200

    Final

--- predictionTranslation.py
+++ predictionTranslation.py
@@ -1,33 +1,21 @@
-from nltk.translate.bleu_score import sentence_bleu
 from keras.models import load_model
 from util import *
-import random
 filename="./output/char2encoding.pkl"
+sentence="I love deep learning"
 
-with open('spa-eng/spa.txt') as f:
-    lines=f.readlines()
-random.shuffle(lines)
+input_token_index,max_encoder_seq_length,num_encoder_tokens,reverse_target_char_index,num_decoder_tokens,target_token_index= getChar2encoding(filename)
 
-test= lines[:30]
-with open('DECODED.txt', 'w') as f2:
-    for i in test:
-        sentence=str(i).split('\t')[0]
-        print(sentence)
+encoder_input_data=encodingSentenceToPredict(sentence,input_token_index,max_encoder_seq_length,num_encoder_tokens) 
+encoder_model= load_model('encoder_modelPredTranslation.h5')
+decoder_model= load_model('decoder_modelPredTranslation.h5')
 
-        #num_encoder_tokens 91 77
-        #saveChar2encoding("char2encoding.pkl",input_token_index,16,71,reverse_target_char_index,num_decoder_tokens,target_token_index)
-        input_token_index,max_encoder_seq_length,num_encoder_tokens,reverse_target_char_index,num_decoder_tokens,target_token_index= getChar2encoding(filename)
+input_seq = encoder_input_data
 
-        encoder_input_data=encodingSentenceToPredict(sentence,input_token_index,max_encoder_seq_length,num_encoder_tokens) #91
-        encoder_model= load_model('encoder_modelPredTranslation.h5')
-        decoder_model= load_model('decoder_modelPredTranslation.h5')
+decoded_sentence=decode_sequence(input_seq,encoder_model,decoder_model,num_decoder_tokens,target_token_index,reverse_target_char_index)
+print('-')
+print('Input sentence:', sentence)
+print('Decoded sentence:', decoded_sentence)
 
-        input_seq = encoder_input_data
-
-        decoded_sentence=decode_sequence(input_seq,encoder_model,decoder_model,num_decoder_tokens,target_token_index,reverse_target_char_index)
-        
-        f2.write(str(sentence),'\t', (str(decoded_sentence)))
-        f2.write("\n")
 
 # BLEU SCORE
 def get_blue_score(filename):
